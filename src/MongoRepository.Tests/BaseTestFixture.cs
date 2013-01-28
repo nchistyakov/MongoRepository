e94ebc8a386a54d2a189f480623a0b104e7edd0e
lucajulian <luca.zulian@live.it>
2013-01-28 20:15:23 +0100

    Tests infrastructure

---
+++ src/MongoRepository.Tests/BaseTestFixture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MongoRepository.Tests
+{
+    public class BaseTestFixture
+    {
+        [TestFixtureSetUp]
+        protected virtual void Setup()
+        {
+        
+        
+        }
+
+        [TestFixtureTearDown]
+        protected virtual void TearDown()
+        {
+        }
+    }
+}
---
+++ src/MongoRepository.Tests/RepositoryTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MongoRepository.Tests
+{
+    [TestFixture]
+    public class RepositoryTests : BaseTestFixture
+    {
+        private class TestClass
+        {
+            public string Id { get; set; }
+            public int IntProperty { get; set; }
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            
+        }
+    }
+}
